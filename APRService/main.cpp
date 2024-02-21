34f09e204e13b1bd45999474d25d59b69555372d
Francis Barney <leo633395@gmail.com>
2024-02-21 10:11:18 -0800

    refactor

--- APRService/main.cpp
+++ APRService/main.cpp
@@ -1,10 +1,19 @@
 #include "aprservice_lua.hpp"
 
+#include <AL/OS/Console.hpp>
+
 int main(int argc, char* argv[])
 {
+	AL::String script_path;
+
+	if (argc == 2)
+		script_path = argv[1];
+	else if (!AL::OS::Console::Write("Script Path: ") || !AL::OS::Console::ReadLine(script_path))
+		return 0;
+
 	if (auto lua = aprservice_lua_init())
 	{
-		aprservice_lua_run_file(lua, argv[1]);
+		aprservice_lua_run_file(lua, script_path);
 		aprservice_lua_deinit(lua);
 	}
 

4439fadee43d0e54546374074ae9ae91ca32ee36
= <=>
2025-08-25 19:10:50 -0700

    APRSERVICE_CALL is now empty on non-win32 builds

--- APRService/api.h
+++ APRService/api.h
@@ -1,15 +1,16 @@
 #ifndef APRSERVICE_API_H
 #define APRSERVICE_API_H
 
-#define APRSERVICE_CALL __cdecl
-
 #if defined(APRSERVICE_WIN32)
+	#define APRSERVICE_CALL __cdecl
+
 	#ifdef APRSERVICE_API
 		#define APRSERVICE_EXPORT __declspec(dllexport)
 	#else
 		#define APRSERVICE_EXPORT __declspec(dllimport)
 	#endif
 #else
+	#define APRSERVICE_CALL   
 	#define APRSERVICE_EXPORT 
 #endif
 
